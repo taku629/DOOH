44994d769e21fc9c2cd36d68a4c7fc7e58f808b2
taku629 <karisutefin73@gmail.com>
2026-05-10 23:10:56 +0900

    Fix playback logger naming

--- src/logger.js
+++ src/logger.js
@@ -1,8 +1,8 @@
-export function loogerPlayhack(videoPath){
-    const time =new Date().toLocaleDateString();
+export function logPlayback(videoPath) {
+    const time = new Date().toLocaleString();
     console.log(`[${time}] playing: ${videoPath}`);
 }
 
 export function logError(error) {
-    
+    console.error("[DOOH Player]", error);
 }
